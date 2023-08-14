21917a4162a593f8b81262b6085dfab5a78d154b
TkachenkoVitaliy <tkachenko.java@yandex.ru>
2023-08-14 11:00:17 +0500

    add ReflectUtil

---
+++ src/main/java/com/poiorm/util/ReflectUtil.java
@@ -0,0 +1,36 @@
+package com.poiorm.util;
+
+import com.poiorm.exception.MappingException;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public final class ReflectUtil {
+
+    private ReflectUtil() {
+    }
+
+    public static <T> T newInstance(Class<T> type) {
+        T instance;
+        try {
+            Constructor<T> constructor = type.getDeclaredConstructor();
+            if (!constructor.canAccess(null)) {
+                constructor.setAccessible(true);
+            }
+            instance = constructor.newInstance();
+        } catch (InvocationTargetException | NoSuchMethodException | InstantiationException | IllegalAccessException e) {
+            throw new MappingException(String.format("Cannot create a new instance of %s", type.getName()), e);
+        }
+        return instance;
+    }
+
+    public static void setFieldValue(Field field, Object value, Object instance) {
+        try {
+            field.setAccessible(true);
+            field.set(instance, value);
+        } catch (IllegalAccessException e) {
+            throw new MappingException(String.format("Unexpected cast type {%s} of field %s", value, field.getName()));
+        }
+    }
+}
